1a22afe8071748f895498315fe3fe98c9f69cf19
treyer <nemereno_by@mail.ru>
2017-02-13 18:51:18 +0300

    some commit

---
+++ model/src/main/java/com/epam/test/model/User.java
@@ -0,0 +1,78 @@
+package com.epam.test.model;
+
+import java.util.Objects;
+
+/**
+ * Created by andrei on 13.2.17.
+ */
+public class User {
+    private int userID;
+    private String login;
+    private String password;
+    private String description;
+
+    public User(int userID, String login, String password, String description) {
+        this.userID = userID;
+        this.login = login;
+        this.password = password;
+        this.description = description;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userID == user.userID &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(description, user.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userID, login, password, description);
+    }
+}
